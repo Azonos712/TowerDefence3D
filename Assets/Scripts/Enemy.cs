e6c224feeddc4d3e418b1337508622baf796fc8f
Бездетный Николай <Nekooolay@mail.ru>
2020-04-12 12:46:09 +0300

    Путевые точки, базовое передвижение противников

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 10;
+    private Transform target;
+    private int wayPointIndex = 0;
+    void Start()
+    {
+        target = Waypoints.points[wayPointIndex];
+    }
+
+    void Update()
+    {
+        Vector3 dir = target.position - transform.position;
+        transform.Translate(dir.normalized * speed * Time.deltaTime, Space.World);
+
+        if (Vector3.Distance(transform.position, target.position) <= 0.4)
+        {
+            GetNextWayPoint();
+        }
+    }
+
+    void GetNextWayPoint()
+    {
+        if (wayPointIndex >= Waypoints.points.Length - 1)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        wayPointIndex++;
+        target = Waypoints.points[wayPointIndex];
+    }
+}
---
+++ Assets/Scripts/Waypoints.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public class Waypoints : MonoBehaviour
+{
+    //массив с путевыми точками
+    public static Transform[] points;
+
+    //при старте получаем все путевые точки и сохраняем их в массив
+    private void Awake()
+    {
+        points = new Transform[transform.childCount];
+        for (int i = 0; i < points.Length; i++)
+        {
+            points[i] = transform.GetChild(i);
+        }
+    }
+}
