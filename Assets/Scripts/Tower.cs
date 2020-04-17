6c11147897c5826a338e650a97e8a619706127cc
Бездетный Николай <Nekooolay@mail.ru>
2020-04-17 12:49:39 +0300

    Скрипт для башни (Захват цели, поворот башни, планвные переходы), тег "Enemy" для противников

--- Assets/Scripts/Tower.cs
+++ Assets/Scripts/Tower.cs
@@ -4,28 +4,62 @@ using UnityEngine;
 
 public class Tower : MonoBehaviour
 {
-    public Transform target;
     public float range = 15f;
+    public string enemyTag = "Enemy";
+    public Transform partToRotate;
+
+    public float turnSpeed = 10f;
+    private Transform target;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        //Запускаем метод с повторением в полсекунды сразу же при вызове Start()
+        InvokeRepeating("UpdateTarget", 0f, 0.5f);
     }
 
     void UpdateTarget()
     {
+        GameObject[] enemies = GameObject.FindGameObjectsWithTag(enemyTag);
+        float shortestDistanse = Mathf.Infinity;
+        GameObject nearestEnemy = null;
+        foreach (var enemy in enemies)
+        {
+            float distanceToEnemy = Vector3.Distance(transform.position, enemy.transform.position);
+            if (distanceToEnemy < shortestDistanse)
+            {
+                shortestDistanse = distanceToEnemy;
+                nearestEnemy = enemy;
+            }
+        }
 
+        if (nearestEnemy != null && shortestDistanse <= range)
+        {
+            target = nearestEnemy.transform;
+        }
+        else
+        {
+            target = null;
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (target == null)
+            return;
+
+        //Захват цели
+        Vector3 dir = target.position - transform.position;
+        Quaternion lookRotation = Quaternion.LookRotation(dir);
+        Vector3 rotation = Quaternion.Lerp(partToRotate.rotation, lookRotation, Time.deltaTime * turnSpeed).eulerAngles;//lookRotation.eulerAngles;
+        partToRotate.rotation = Quaternion.Euler(0f, rotation.y, 0f);
     }
 
     private void OnDrawGizmosSelected()
     {
+        //Отображение дистанции башни в редакторе
         Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position,range);
+        Gizmos.DrawWireSphere(transform.position, range);
     }
 }
