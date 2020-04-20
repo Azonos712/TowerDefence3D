f74f373773ae01aef0f805223128687bda545ea8
Бездетный Николай <Nekooolay@mail.ru>
2020-04-20 15:36:26 +0300

    Выделение платформы другим цветом при наведении

---
+++ Assets/Platform.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+    public Color emissionColor;
+
+    private Renderer r;
+    private Color startColor;
+    private void Start()
+    {
+        r = GetComponent<Renderer>();
+        startColor = r.material.color;
+    }
+    private void OnMouseEnter()
+    {
+        r.material.color = emissionColor;
+    }
+    private void OnMouseExit()
+    {
+        r.material.color = startColor;
+    }
+}
