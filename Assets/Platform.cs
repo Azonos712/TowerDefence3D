73829312b992a61d9612fe9ea0ad409cc3e573a3
Бездетный Николай <Nekooolay@mail.ru>
2020-04-20 16:41:43 +0300

    Реализация паттерна Singleton в классе управления строительством, для доступа к нему из любой точки кода. Создание башни на платформе при клике мышкой

--- Assets/Platform.cs
+++ Assets/Platform.cs
@@ -1,17 +1,29 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Platform : MonoBehaviour
 {
     public Color emissionColor;
 
+    private Vector3 positionOffset = new Vector3(0f, 0.5f, 0f);
+    private GameObject installedTower;
     private Renderer r;
     private Color startColor;
     private void Start()
     {
         r = GetComponent<Renderer>();
         startColor = r.material.color;
+    }
+    private void OnMouseDown()
+    {
+        if (installedTower != null)
+        {
+            Debug.Log("TODO: Display on screen!");
+            return;
+        }
+
+        //Строим башню
+        GameObject towerToBuild = BuildManager.instance.GetTowerToBuild();
+        installedTower = Instantiate(towerToBuild, transform.position + positionOffset, transform.rotation);
     }
     private void OnMouseEnter()
     {
