e586a0847748318c4b6b57aff5ce957e82cb20d4
Бездетный Николай <Nekooolay@mail.ru>
2020-04-22 11:34:10 +0300

    Перемещение камеры при помощи wasd и перетаскивания мыши по краям

---
+++ Assets/CameraController.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float speed = 30f;
+    public float borderOffset = 10f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //TODO: сцена и всё объекты развёрнуты по y на 90, поэтому небольшая путаница с направлениями
+        if (Input.GetKey("w") || Input.mousePosition.y >= Screen.height - borderOffset)
+        {
+            transform.Translate(Vector3.left * speed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("s") || Input.mousePosition.y <= borderOffset)
+        {
+            transform.Translate(Vector3.right * speed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("d") || Input.mousePosition.x >= Screen.width - borderOffset)
+        {
+            transform.Translate(Vector3.forward * speed * Time.deltaTime, Space.World);
+        }
+        if (Input.GetKey("a") || Input.mousePosition.x <= borderOffset)
+        {
+            transform.Translate(Vector3.back * speed * Time.deltaTime, Space.World);
+        }
+    }
+}
