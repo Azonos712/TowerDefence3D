79f26c6aa2a75eba6adb45de6b918908d129a487
Nikolay <nekooolay@mail.ru>
2020-05-14 16:15:16 +0300

    Система магазина башен, UI магазина, иконка башни

---
+++ Assets/Shop.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void PurchaseStandartTurret()
+    {
+        buildManager.SetTowerToBuild(buildManager.standartTowerPrefab);
+    }
+
+    public void PurchaseAnotherTurret()
+    {
+        buildManager.SetTowerToBuild(buildManager.anotherTowerPrefab);
+    }
+}
