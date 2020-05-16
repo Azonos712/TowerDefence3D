43b6c89805cf1e2ded74ac2ae55dec4ab4fd2df7
Nikolay <nekooolay@mail.ru>
2020-05-16 12:53:45 +0300

    Коллайдер для противника, радиус взрыва снаряда

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -4,6 +4,7 @@ public class Bullet : MonoBehaviour
 {
     [Header("Attributes")]
     public float speed = 70f;
+    public float explosionRadius = 0f;
     [Header("Unity Setup Fields")]
     public GameObject impactEffect;
     private Transform target;
@@ -35,13 +36,48 @@ public class Bullet : MonoBehaviour
         //Далее перемножаем этот вектор на скорость и разность времени между кадрами
         //И перемещаем в мировых координатах при помощи вызванного метода
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
+
+        transform.LookAt(target);
     }
 
     void HitTarget()
     {
         GameObject effectIns = Instantiate(impactEffect, transform.position, transform.rotation);
         Destroy(effectIns, 2f);
-        Destroy(target.gameObject);
+
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         target = null;
+    }
+
+    void Explode()
+    {
+        //получаем список объектов задетых сферой
+        Collider[] hitObjects = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var obj in hitObjects)
+        {
+            if (obj.tag == "Enemy")
+            {
+                Damage(obj.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawSphere(transform.position, explosionRadius);
     }
 }
